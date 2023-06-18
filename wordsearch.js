f6d84859d88b31f9fdfbab1b0eeedf2e22f6959a
Devvaru <syeraru@gmail.com>
2023-06-18 16:48:08 -0600

    Create word search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  // check each row for word
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // transpose the rows to columns
+  let verticalJoin = '';
+  for (let col = 0; col < letters[0].length; col++) {
+    for (let row = 0; row < letters.length; row++) {
+      verticalJoin += letters[row][col];
+    }
+  }
+  // check each new row for word
+  for (let v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
